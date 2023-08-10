4c676cfb96a5d7bb7f2aee5ad10e65801b2e0071
VictorAgha <victoralagha.va@gmail.com>
2023-08-10 22:08:19 +0300

    adding follow

--- instagram-frontend/src/App.js
+++ instagram-frontend/src/App.js
@@ -3,6 +3,8 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LoginPage from './components/forms/signin-form/Login';
 import RegisterPage from './components/forms/registration-form/Register';
 import Landing from './components/sidebar/Sidebar';
+import Followers from './components/followers';
+import Following from './components/following';
 import './App.css';
 
 function App() {
@@ -12,6 +14,8 @@ function App() {
         <Route path="/" element={<LoginPage />}></Route>
         <Route path="/register" element={<RegisterPage />}></Route>
         <Route path='/landing' element={<Landing />}></Route>
+        <Route path='/following' element={<Following />}></Route>
+        <Route path='/followers' element={<Followers />}></Route>
       </Routes>
     </Router>
   );
--- instagram-frontend/src/components/landing-page/Landing.jsx
+++ instagram-frontend/src/components/landing-page/Landing.jsx
@@ -1,10 +1,39 @@
 import React from 'react';
 import Landing from '../sidebar/Sidebar';
+import Card from '../card';
 
 const Landing = () => {
+    const [posts, setPosts] = useState([]);
+
+    useEffect(() => {
+        getPosts();
+    }, []);
+
+    const getPosts = async () => {
+        try {
+            const response = await axios.get(
+                'http://127.0.0.1:8000/api/getposts',
+                {
+                    headers: {
+                        Authorization: `Bearer ${authtoken}`,
+                        'Content-Type': 'application/json',
+                    },
+                }
+            );
+            setPosts(response.data.posts);
+        } catch (error) {
+            console.log('Error fetching posts:', error);
+        }
+    };
+
     return (
         <div>
             <Sidebar />
+            <ul className="cards">
+                {posts.map((post) => (
+                    <Card post_id={post.id} key={post.id} image_url={post.image_url} likes_count={post.likes_count} />
+                ))}
+            </ul>
         </div >
 
     );
